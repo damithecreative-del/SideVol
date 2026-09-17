c45436d64c83b9cc8a7abfc2a57f13901e5f98f8
damithecreative-del <damithecreative@gmail.com>
2026-09-17 01:17:20 +0100

    Add Quick Settings volume-down tile

---
+++ app/src/main/java/com/damithecreative/sidevol/VolumeDownTileService.java
@@ -0,0 +1,29 @@
+package com.damithecreative.sidevol;
+
+import android.media.AudioManager;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class VolumeDownTileService extends TileService {
+    @Override public void onStartListening() {
+        super.onStartListening();
+        updateTile();
+    }
+
+    @Override public void onClick() {
+        AudioManager audio = (AudioManager) getSystemService(AUDIO_SERVICE);
+        if (audio != null) {
+            audio.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+        }
+        updateTile();
+    }
+
+    private void updateTile() {
+        Tile tile = getQsTile();
+        if (tile != null) {
+            tile.setLabel("SideVol −");
+            tile.setState(Tile.STATE_ACTIVE);
+            tile.updateTile();
+        }
+    }
+}
